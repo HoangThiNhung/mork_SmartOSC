78c3b42148acb1a19a4505fcb9aafe53a21a17a5
nhunght57 <nhunght1108@gmail.com>
2015-09-12 17:58:31 +0700

    config

---
+++ config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => '',
+        'secret' => '',
+    ],
+
+    'mandrill' => [
+        'secret' => '',
+    ],
+
+    'ses' => [
+        'key'    => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+    ],
+
+    'stripe' => [
+        'model'  => App\User::class,
+        'key'    => '',
+        'secret' => '',
+    ],
+
+    'facebook' => [
+        'client_id' => '1626145747637990',
+        'client_secret' => 'a22e24ff6d763135e06c3b023b9ffb95',
+        'redirect' => 'http://your-callback-url',
+    ],
+
+];
